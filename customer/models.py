252bbf048224179bef435f31f6093a6be78b63a1
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-02-01 16:56:38 +0200

    correct customer model by using abstractuser default fields instead of write myself

--- customer/models.py
+++ customer/models.py
@@ -1,12 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class Customer(models.Model):
-    Email = models.CharField(max_length=50)
-    First_name = models.CharField(max_length=50)
-    Last_name = models.CharField(max_length=50)
-    Password = models.CharField(max_length=128)
-    is_staff = models.BooleanField(default=False)
+class User(AbstractUser):
+    email = models.EmailField(max_length=50, unique=True)
 
-    def str(self):
-        return f"{self.First_name} {self.Last_name} ({self.Email})"
+    def __str__(self):
+        return self.email
