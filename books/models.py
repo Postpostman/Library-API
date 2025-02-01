84f55b615bfecdc3f3c181c79f64dcca6c436f13
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-02-01 13:40:25 +0200

    divide written models to their owns apps in Library-API

--- books/models.py
+++ books/models.py
@@ -24,28 +24,3 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.Title} by {self.Author}"
-
-
-class Customer(models.Model):
-    Email = models.CharField(max_length=50)
-    First_name = models.CharField(max_length=50)
-    Last_name = models.CharField(max_length=50)
-    Password = models.CharField(max_length=128)
-    is_staff = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"{self.First_name} {self.Last_name} ({self.Email})"
-
-
-class Borrowing(models.Model):
-    Borrow_date = models.DateField()
-    Expected_return_date = models.DateField()
-    Actual_return_day = models.DateField()
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
-    customer = models.ForeignKey(
-        Customer, on_delete=models.CASCADE, related_name="borrowings"
-    )
-    is_returned = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"Borrowing of {self.book} by {self.customer}"
