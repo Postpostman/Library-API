337c7c8bb743e6af9a56b03ca816cc20d7e8a889
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-02-01 17:45:58 +0200

    write urls for user model

---
+++ customer/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+
+from customer.views import UserViewSet
+
+router = DefaultRouter()
+router.register("", UserViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "customer"
