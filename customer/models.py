8cafa0ae08a82280e20dd63ad7e7c9468ef259fb
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-02-01 16:47:57 +0200

    create customer model

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    Email = models.CharField(max_length=50)
+    First_name = models.CharField(max_length=50)
+    Last_name = models.CharField(max_length=50)
+    Password = models.CharField(max_length=128)
+    is_staff = models.BooleanField(default=False)
+
+    def str(self):
+        return f"{self.First_name} {self.Last_name} ({self.Email})"
