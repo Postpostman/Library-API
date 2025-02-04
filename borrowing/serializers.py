a33c01cb93d516ea1eef325084cbfbc2fa5d89e2
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-02-04 23:02:27 +0200

    update serializers and views in borrowing app

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from borrowing.models import Borrowing
+from books.serializers import BookSerializer
 
 
-class BorrowingSerializer(serializers.ModelSerializer):
+class BorrowingReadSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source="user.id")
+    book = BookSerializer(read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = "__all__"
+
+
+class BorrowingWriteSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source="user.id")
 
     class Meta:
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,12 +1,21 @@
 from rest_framework import viewsets, permissions
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingSerializer
+from borrowing.serializers import BorrowingReadSerializer, BorrowingWriteSerializer
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
-    queryset = Borrowing.objects.all()
-    serializer_class = BorrowingSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Borrowing.objects.all()
+        return Borrowing.objects.filter(user=user)
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return BorrowingReadSerializer
+        return BorrowingWriteSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
