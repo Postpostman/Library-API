1bca38a7e212b58b08e53e7a100a8739a94fe296
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-02-04 23:13:11 +0200

    add create serializer & book validation

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,20 +1,45 @@
 from rest_framework import serializers
 from borrowing.models import Borrowing
-from books.serializers import BookSerializer
+from books.models import Book
 
 
 class BorrowingReadSerializer(serializers.ModelSerializer):
+    """Read-serializer for viewing the list and details of borrowings."""
     user = serializers.ReadOnlyField(source="user.id")
-    book = BookSerializer(read_only=True)
+    book = serializers.SerializerMethodField()
 
     class Meta:
         model = Borrowing
         fields = "__all__"
 
+    def get_book(self, obj):
+        """Returns detailed information about the book"""
+        return {
+            "id": obj.book.id,
+            "title": obj.book.title,
+            "author": obj.book.author,
+            "daily_fee": obj.book.daily_fee,
+        }
 
-class BorrowingWriteSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source="user.id")
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    """Write-serializer for creating a new loan."""
 
     class Meta:
         model = Borrowing
-        fields = "__all__"
+        fields = ["borrow_date", "expected_return_date", "book"]
+
+    def validate_book(self, book):
+        """Checks if the book is available."""
+        if book.inventory < 1:
+            raise serializers.ValidationError("This book is out of stock.")
+        return book
+
+    def create(self, validated_data):
+        """Creates a loan, reduces `inventory` and adds a user."""
+        book = validated_data["book"]
+        book.inventory -= 1
+        book.save()
+
+        borrowing = Borrowing.objects.create(**validated_data)
+        return borrowing
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,21 +1,24 @@
 from rest_framework import viewsets, permissions
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingReadSerializer, BorrowingWriteSerializer
+from borrowing.serializers import BorrowingReadSerializer, BorrowingCreateSerializer
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
+        """Administrators see all loans, users see only their own."""
         user = self.request.user
         if user.is_staff:
             return Borrowing.objects.all()
         return Borrowing.objects.filter(user=user)
 
     def get_serializer_class(self):
-        if self.action in ["list", "retrieve"]:
-            return BorrowingReadSerializer
-        return BorrowingWriteSerializer
+        """Selects the appropriate serializer depending on the action."""
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingReadSerializer
 
     def perform_create(self, serializer):
+        """Assigns `user` before creation."""
         serializer.save(user=self.request.user)
