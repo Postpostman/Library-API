e6b27503870aa9816f9c2e2dbc6f9f75f9808e84
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-02-09 18:28:57 +0200

    protect money_to_pay from editing

--- payment/serializers.py
+++ payment/serializers.py
@@ -6,3 +6,4 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
+        read_only_fields = ["money_to_pay", "status", "session_url", "session_id", "created_at"]
