6a91cd4fd00f6a686e2bc11d235d9227402770aa
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-02-09 18:26:55 +0200

    add created_at to payment models

--- payment/models.py
+++ payment/models.py
@@ -20,11 +20,12 @@ class Payment(models.Model):
         max_length=10, choices=PaymentType.choices, default=PaymentType.PAYMENT
     )
     borrowing = models.ForeignKey(
-        Borrowing, on_delete=models.CASCADE, related_name="payments"
+        Borrowing, on_delete=models.PROTECT, related_name="payments"
     )
     session_url = models.URLField()
     session_id = models.CharField(max_length=255, unique=True)
     money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Payment {self.id} - {self.status} - ${self.money_to_pay}"
