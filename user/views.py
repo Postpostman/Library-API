4ddaaacb2ed898254e20f716e6cfc928204d9862
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-02-09 14:47:05 +0200

    update views for user by adding register view and updating user profile viewby add get method

--- user/views.py
+++ user/views.py
@@ -1,20 +1,41 @@
-from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from user.serializers import (
+    RegisterUserSerializer,
+    UserSerializer,
+    TokenObtainSerializer,
+)
 from user.models import User
-from user.serializers import UserSerializer
-from rest_framework import generics, viewsets
-from user.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class RegisterUserView(APIView):
+    def post(self, request):
+        serializer = RegisterUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(
+                {"message": "User created successfully"}, status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfileView(APIView):
     permission_classes = [IsAuthenticated]
 
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
 
-class UserProfileView(generics.RetrieveUpdateAPIView):
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    def put(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def get_object(self):
-        return self.request.user
+
+class TokenObtainView(TokenObtainPairView):
+    serializer_class = TokenObtainSerializer
